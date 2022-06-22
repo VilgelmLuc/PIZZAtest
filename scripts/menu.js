17a219858563c2464921089af476753c492d1e59
Ivan Lutsenko <ivanluc2015@gmail.com>
2022-06-22 11:44:00 +0300

    99% complete burger

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$('.menu-burger__header').click(function(){
         $('.menu-burger__header').toggleClass('open-menu');
-        $('.navbar').toggleClass('open-menu');
+        $('.navbar__mobile').toggleClass('open-menu');
         $('body').toggleClass('fixed-page');
 	});
 });
